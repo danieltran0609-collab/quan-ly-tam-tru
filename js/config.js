be50fd7e20c5d17ad2b10c0aa9c49dbe764d42e2
danieltran0609-collab <daniel.tran0609@gmail.com>
2026-09-24 09:13:50 +0700

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,5 @@
+// Cấu hình kết nối (không chứa bí mật: URL máy chủ vẫn yêu cầu đăng nhập Google hợp lệ).
+window.TAMTRU_CONFIG = {
+  API_URL: 'https://script.google.com/macros/s/AKfycbx7Y_2DYsn8UY6Q3h_h59scbnyRasSc0I7SEYDXRQkZf0jsnXGIahJMjCwWPdRFakYeEQ/exec',
+  GOOGLE_CLIENT_ID: '248222794615-suvcsan752tclpqr2gfv2l5hi3ijasj3.apps.googleusercontent.com'
+};
